8c4efa01e1565e9f630249f3319873bafc1a7721
Аббакумов Евгений <abbevg@gmail.com>
2014-09-28 18:18:07 +0400

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+
+gulp.task('coffee', function(){
+	gulp.src('scripts/*.coffee')
+		.pipe(coffee({bare: true}).on('error', gutil.log))
+		.pipe(gulp.dest('scripts/js/'))
+});
+
+gulp.task('default', ['coffee']);
+
+gulp.task('watch', function(){
+	gulp.watch('scripts/coffee/*.coffee', ['coffee']);
+});
