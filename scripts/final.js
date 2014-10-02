347d29f1ad23beb787133558be3db307a372ea6f
Аббакумов Евгений <abbevg@gmail.com>
2014-10-02 23:20:35 +0400

    added cell class

--- scripts/final.js
+++ scripts/final.js
@@ -1,2 +1,2 @@
 var x,z;x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],z="test fucking string";
-var xyz,y;y=[1,2,3,4,5,6,7,8,9,10,11,12],xyz="Gulp is amazing";
+var Cell;Cell=function(){function t(t){this.setConfig(t)}return t.prototype.isFree=function(){},t.prototype.isOccupied=function(){},t.prototype.getConfig=function(){return{x:this.x,y:this.y,status:this.status}},t.prototype.setConfig=function(t){this.x=t.x,this.y=t.y,this.status=t.status},t}();
